8f7a2becb25f300515d65cc9f6e49641af183178
Praveen Kumaar <actionking148@gmail.com>
2020-03-20 17:35:15 +0530

    fix

--- src/main/java/com/revature/faculty/service/FacultyServiceImpl.java
+++ src/main/java/com/revature/faculty/service/FacultyServiceImpl.java
@@ -19,43 +19,41 @@ import com.revature.faculty.util.Message;
 
 @Service
 public class FacultyServiceImpl implements FacultyService {
-	
-	
 
 	@Autowired
 	private FacultyDao facultyDao;
-	
+
 	@Transactional
 	@Override
-	public List<Faculty> get() throws ServiceException{	
-		List<Faculty> list=new ArrayList<Faculty>();
+	public List<Faculty> get() throws ServiceException {
+		List<Faculty> list = new ArrayList<Faculty>();
 		try {
-			list=facultyDao.get();
-			if(list.isEmpty()) {
+			list = facultyDao.get();
+			if (list.isEmpty()) {
 				throw new ServiceException(Message.NO_RECORD);
 			}
-		}catch(DBException e) {
+		} catch (DBException e) {
 			System.out.println(e.getMessage());
 		}
 		return list;
 	}
-	
+
 	@Transactional
 	@Override
 	public Faculty get(Long id) throws ServiceException {
 		Faculty fac = new Faculty();
 		try {
-			fac=facultyDao.get(id);
-			if(fac == null) {
+			fac = facultyDao.get(id);
+			if (fac == null) {
 				throw new ServiceException(Message.UNABLE_TO_FIND);
 			}
-		}catch(DBException e) {
+		} catch (DBException e) {
 			System.out.println(e.getMessage());
-			
+
 		}
 		return fac;
 	}
-	
+
 	@Transactional
 	@Override
 	public void save(InsertFacultyDto dto) throws DBException {
@@ -63,78 +61,74 @@ public class FacultyServiceImpl implements FacultyService {
 		Organization org = new Organization();
 		Roles role = new Roles();
 		try {
-		if(dto.getId() == null) {
-			fac.setCreatedon(dto.getCreatedon());
-			System.out.println("Object Does Not Exists");
-		}
-		else {
-			fac = facultyDao.get(dto.getId());
-			fac.setModifiedon(dto.getModifiedon());
-			System.out.println("Object Exists");
-			fac.setId(dto.getId());
-		}
-		
-		fac.setEmployee_id(dto.getEmployee_id());
-		org.setId(dto.getInstitution_id());
-		fac.setOrg(org);
-		fac.setFirst_name(dto.getFirst_name());
-		fac.setLast_name(dto.getLast_name());
-		fac.setDob(dto.getDob());
-		fac.setEmail(dto.getEmail());
-		fac.setMobile_no(dto.getMobile_no());
-		role.setId(dto.getRole_id());
-		fac.setRoles(role);
-		Integer id=dto.getEmployee_id();
-		String fname=dto.getFirst_name();
-		String lname=dto.getLast_name();
-		Date dob=dto.getDob();
-		String email=dto.getEmail();
-		Long mobilenumber=dto.getMobile_no();
-		
-		if( fname==null || lname==null || dob==null || email==null || mobilenumber==null) {
-			
-			 throw new DBException(Message.UNABLE_TO_INSERT);
-		 }
-		 facultyDao.save(fac);
-		 
-		}catch (DBException e) {
+			if (dto.getId() == null) {
+				fac.setCreatedon(dto.getCreatedon());
+				System.out.println("Object Does Not Exists");
+			} else {
+				fac = facultyDao.get(dto.getId());
+				fac.setModifiedon(dto.getModifiedon());
+				System.out.println("Object Exists");
+				fac.setId(dto.getId());
+			}
+
+			fac.setEmployee_id(dto.getEmployee_id());
+			org.setId(dto.getInstitution_id());
+			fac.setOrg(org);
+			fac.setFirst_name(dto.getFirst_name());
+			fac.setLast_name(dto.getLast_name());
+			fac.setDob(dto.getDob());
+			fac.setEmail(dto.getEmail());
+			fac.setMobile_no(dto.getMobile_no());
+			role.setId(dto.getRole_id());
+			fac.setRoles(role);
+			Integer id = dto.getEmployee_id();
+			String fname = dto.getFirst_name();
+			String lname = dto.getLast_name();
+			Date dob = dto.getDob();
+			String email = dto.getEmail();
+			Long mobilenumber = dto.getMobile_no();
+
+			if (fname == null || lname == null || dob == null || email == null || mobilenumber == null) {
+
+				throw new DBException(Message.UNABLE_TO_INSERT);
+			}
+			facultyDao.save(fac);
+
+		} catch (DBException e) {
 			System.out.println(e.getMessage());
 		}
 	}
-	
-	
-	
+
 	@Transactional
 	@Override
-	public void delete(Long id) throws ServiceException{
+	public void delete(Long id) throws ServiceException {
 		Faculty fac = new Faculty();
 		try {
-			fac =facultyDao.get(id);
-			if(fac != null) {
+			fac = facultyDao.get(id);
+			if (fac != null) {
 				facultyDao.delete(id);
-			}
-			else {
+			} else {
 				throw new ServiceException(Message.UNABLE_TO_DELETE);
 			}
-		}catch (DBException e) {
+		} catch (DBException e) {
 			System.out.println(e.getMessage());
 		}
-		 
-		
+
 	}
+
 	@Override
 	public List<Faculty> getByInstitution(Long id) throws ServiceException {
 		List<Faculty> list = new ArrayList<Faculty>();
 		try {
-			list=facultyDao.getByInstitution(id);
-			if(list.isEmpty()) {
-				throw new ServiceException(Message.NO_INSTITUTION_AVAILABLE);
+			list = facultyDao.getByInstitution(id);
+			if (list.isEmpty()) {
+				throw new ServiceException(Message.NO_FAULTY_AVAILABLE);
 			}
-		}catch(DBException e) {
+		} catch (DBException e) {
 			System.out.println(e.getMessage());
 		}
 		return list;
-	
+
 	}
 
 }
--- src/main/java/com/revature/faculty/service/RolesServiceImpl.java
+++ src/main/java/com/revature/faculty/service/RolesServiceImpl.java
@@ -1,11 +1,11 @@
 package com.revature.faculty.service;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.revature.faculty.Exception.DBException;
 import com.revature.faculty.Exception.ServiceException;
@@ -13,75 +13,74 @@ import com.revature.faculty.dao.RolesDao;
 import com.revature.faculty.model.Roles;
 import com.revature.faculty.util.Message;
 
-import org.springframework.transaction.annotation.Transactional;
 @Service
 public class RolesServiceImpl implements RolesService {
 
 	@Autowired
 	private RolesDao rolesDao;
-	
+
 	@Transactional
 	@Override
 	public List<Roles> get() throws ServiceException {
-		List<Roles> list=new ArrayList<Roles>();
+		List<Roles> list = new ArrayList<Roles>();
 		try {
-			list=rolesDao.get();
-			if(list.isEmpty()) {
+			list = rolesDao.get();
+			if (list.isEmpty()) {
 				throw new ServiceException(Message.NO_RECORD);
 			}
-		}catch(DBException e) {
+		} catch (DBException e) {
 			System.out.println(e.getMessage());
-			
+
 		}
 		return list;
 	}
+
 	@Transactional
 	@Override
 	public Roles get(Long id) throws ServiceException {
 		Roles role = new Roles();
 		try {
 			role = rolesDao.get(id);
-			if(role == null) {
+			if (role == null) {
 				throw new ServiceException(Message.UNABLE_TO_FIND_ROLE);
 			}
-		}catch(DBException e) {
+		} catch (DBException e) {
 			System.out.println(e.getMessage());
 		}
-		
+
 		return role;
 	}
+
 	@Transactional
 	@Override
 	public void save(Roles role) throws DBException {
 		try {
 			String name = role.getName();
-			if(name == null) {
+			if (name == null) {
 				throw new DBException(Message.UNABLE_TO_INSERT);
 			}
 			rolesDao.save(role);
-			}catch (DBException e) {
-				System.out.println(e.getMessage());
-			}
-		
+		} catch (DBException e) {
+			System.out.println(e.getMessage());
+		}
 
 	}
+
 	@Transactional
 	@Override
 	public void delete(Long id) throws ServiceException {
 		Roles role = new Roles();
 		try {
-			role=rolesDao.get(id);
-			if(role != null) {
+			role = rolesDao.get(id);
+			if (role != null) {
 				rolesDao.delete(id);
-			}
-			else {
+			} else {
 				throw new ServiceException(Message.UNABLE_TO_DELETE_ROLE);
 			}
-		}catch(DBException e) {
+		} catch (DBException e) {
 			System.out.println(e.getMessage());
 		}
 
 	}
-	
 
 }
